8dbbaf43c57d56401edd07510709d8024d6eded9
lyeiva86 <ulul10039@gmail.com>
2024-05-31 01:59:16 +0700

    fix: view homepage user

--- lib/app/modules/homepage_user/views/homepage_user_view.dart
+++ lib/app/modules/homepage_user/views/homepage_user_view.dart
@@ -5,14 +5,10 @@ import 'package:get/get.dart';
 import '../controllers/homepage_user_controller.dart';
 
 class HomepageUserView extends GetView<HomepageUserController> {
-  const HomepageUserView({Key? key}) : super(key: key);
+  const HomepageUserView({super.key});
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('HomepageUserView'),
-        centerTitle: true,
-      ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
@@ -20,28 +16,28 @@ class HomepageUserView extends GetView<HomepageUserController> {
             child: Column(
               children: [
                 Image.asset('assets/images/logo.png', height: 125),
-                SizedBox(
+                const SizedBox(
                   height: 30,
                 ),
-                Text(
+                const Text(
                   "J-CARE",
                   style: TextStyle(
                     fontSize: 40,
                     fontFamily: 'Poppins-ExtraBold',
                     fontWeight: FontWeight.w700,
-                    color: const Color(0xFF1D3557),
+                    color: Color(0xFF1D3557),
                   ),
                 ),
-                SizedBox(
+                const SizedBox(
                   height: 10,
                 ),
-                Padding(
+                const Padding(
                   padding: EdgeInsets.fromLTRB(60, 0, 60, 0),
                   child: Text(
                     textAlign: TextAlign.center,
                     "Selamat datang para pengguna, gunakan hak anda untuk melaporkan kerusakan fasilitas di sekitar anda!",
                     style: TextStyle(
-                      color: const Color(0xFF1D3557),
+                      color: Color(0xFF1D3557),
                       fontFamily: 'Poppins-Bold',
                       fontSize: 24
                     ),
