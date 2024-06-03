7600fc748c4f1ad6dd6706134ccb50e2a4c7ea09
yusril maqoshidana <yusrilkingsr07@gmail.com>
2024-06-03 22:28:55 +0700

    fix: bottom navigasi

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,5 +1,4 @@
 import 'package:aduan/app/modules/akun_user/views/akun_user_view.dart';
-import 'package:aduan/app/modules/homepage_user/views/homepage_user_view.dart';
 import 'package:aduan/app/modules/pelaporan_user/views/pelaporan_user_view.dart';
 import 'package:aduan/app/modules/semua_laporan_user/views/semua_laporan_user_view.dart';
 import 'package:get/get.dart';
@@ -8,9 +7,8 @@ class HomeController extends GetxController {
   var selectedIndex = 0.obs;
 
   final views = [
-    const HomepageUserView(),
-    const PelaporanUserView(),
     const SemuaLaporanUserView(),
+    const PelaporanUserView(),
     const AkunUserView(),
   ];
 }
--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -16,12 +16,10 @@ class HomeView extends GetView<HomeController> {
             backgroundColor: const Color.fromRGBO(29, 53, 87, 1),
             title: Text(
               controller.selectedIndex.value == 0
-                  ? 'Hompage'
+                  ? 'Homepage'
                   : controller.selectedIndex.value == 1
                       ? 'Laporan'
-                      : controller.selectedIndex.value == 2
-                          ? 'Semua Laporan'
-                          : 'Akun',
+                      : 'Akun',
             ),
             foregroundColor: Colors.white,
             centerTitle: true,
@@ -36,32 +34,29 @@ class HomeView extends GetView<HomeController> {
       }),
       bottomNavigationBar: Obx(() {
         return BottomNavigationBar(
-          // Set the background color using `backgroundColor`
           backgroundColor: const Color.fromRGBO(29, 53, 87, 1),
-          fixedColor: const Color.fromRGBO(29, 53, 87, 1),
           currentIndex: controller.selectedIndex.value,
           onTap: (index) {
             controller.selectedIndex.value = index;
           },
+          selectedItemColor: Colors.white,
+          unselectedItemColor: Colors.grey,
+          selectedLabelStyle: const TextStyle(color: Colors.white),
+          unselectedLabelStyle: const TextStyle(color: Colors.grey),
           items: const [
             BottomNavigationBarItem(
-              icon: Icon(Icons.home, color: Color.fromARGB(255, 68, 68, 68)),
-              activeIcon: Icon(Icons.home, color:  Color.fromRGBO(29, 53, 87, 1)),
+              icon: Icon(Icons.home),
+              activeIcon: Icon(Icons.home, color: Colors.white),
               label: 'Beranda',
             ),
             BottomNavigationBarItem(
-              icon: Icon(Icons.report_gmailerrorred, color: Color.fromARGB(255, 68, 68, 68)),
-              activeIcon: Icon(Icons.report_gmailerrorred, color:  Color.fromRGBO(29, 53, 87, 1)),
+              icon: Icon(Icons.report_gmailerrorred),
+              activeIcon: Icon(Icons.report_gmailerrorred, color: Colors.white),
               label: 'Laporan',
             ),
             BottomNavigationBarItem(
-              icon: Icon(Icons.document_scanner_outlined, color: Color.fromARGB(255, 68, 68, 68)),
-              activeIcon: Icon(Icons.document_scanner_outlined, color:  Color.fromRGBO(29, 53, 87, 1)),
-              label: 'Semua Laporan',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.person, color: Color.fromARGB(255, 68, 68, 68)),
-              activeIcon: Icon(Icons.person, color:  Color.fromRGBO(29, 53, 87, 1)),
+              icon: Icon(Icons.person),
+              activeIcon: Icon(Icons.person, color: Colors.white),
               label: 'Akun',
             ),
           ],
